4c8e229aa5d0800a06e385df4adf71fafd9f0b10
Mary Joy Labadan <141330072+mryjybrcia@users.noreply.github.com>
2023-09-26 21:48:26 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", () => {
+  document.querySelector(".success").classList.remove("hidden");
+  document.querySelector(".signInContainer").classList.add("hidden");
+  document.querySelector(".imgSignIn").classList.add("hidden");
+});
